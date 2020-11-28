359860e3d132ce952e5b9c5dd3937e3f2ca10904
padmashri <padmashri.mallapur@gmail.com>
2020-11-28 18:59:44 -0500

    Declare and reference simple variable

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+val name = "padma"
+fun main(args: Array<String>) {
+    var num1 = 45
+    println("Initial value of num1 $num1")
+    num1++
+    println("The new value of num1 is $num1")
+
+    val num2:Double = 5.toDouble()
+    println("The value of num2 is $num2")
+
+    println("My name is $name")
+}
