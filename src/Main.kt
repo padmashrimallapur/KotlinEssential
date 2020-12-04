97d22af68ce81e2aadbbdb36274e0e8f30b1c50e
padmashri <padmashri.mallapur@gmail.com>
2020-12-04 11:47:01 -0500

    Constants in companion object

--- src/Main.kt
+++ src/Main.kt
@@ -1,34 +1,7 @@
+import Constants.Companion.RED
 import kotlin.math.abs
 import kotlin.math.round
 
 fun main(args: Array<String>) {
-    var aString = "Hello!"
-    println(aString)
-
-    val empty = String()
-    println("'$empty'")
-
-    val charArray:CharArray = aString.toCharArray()
-    println(charArray.toList())
-
-    val byteArray:ByteArray = aString.toByteArray()
-    println(byteArray.toList())
-
-    aString += " and welcome!!"
-    println(aString)
-
-    val len:Int = aString.length
-    for(i:Int in 0 until  len){
-       val c:Char = aString.get(i)
-        println(c)
-    }
-
-    val p:Int = aString.indexOf("w")
-    val subString:String = aString.substring(p)
-    println("Substring : $subString")
-
-    val string2:String = aString.toUpperCase()
-    val match = aString.equals(string2, true)
-
-    println("Match : $match")
+    println("The color is $RED")
 }
